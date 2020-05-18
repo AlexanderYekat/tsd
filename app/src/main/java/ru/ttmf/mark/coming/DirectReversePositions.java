8d9b92e20f97ba814ada7cea11abcef187290129
Rafael Isakov <rafaelqa444@gmail.com>
2020-05-18 12:23:21 +0300

    Прямой, обратный приход

---
+++ app/src/main/java/ru/ttmf/mark/coming/DirectReversePositions.java
@@ -0,0 +1,37 @@
+package ru.ttmf.mark.coming;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import ru.ttmf.mark.R;
+import ru.ttmf.mark.common.BaseFragment;
+import ru.ttmf.mark.common.DataType;
+
+
+public class DirectReversePositions extends BaseFragment {
+    @BindView(R.id.direct)
+    Button directButton;
+    @BindView(R.id.reverse)
+    Button reverseButton;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_direct_reverse_coming;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        directButton.setOnClickListener(v -> showFragment(SearchFragment.createInstance(DataType.TTN_DIRECT),
+                getString(R.string.direct_coming), true, true));
+        reverseButton.setOnClickListener(view1 -> showFragment(SearchFragment.createInstance(DataType.TTN_REVERSE),
+                getString(R.string.reverse_coming), true, true));
+        return view;
+    }
+}
