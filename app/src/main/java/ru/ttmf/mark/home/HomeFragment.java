aeda5b507cefe9fdcc7c043d09137ca38f1c49ef
Albert Danilov <albert95danilov@gmail.com>
2020-08-24 10:46:00 +0300

    ver 1.26 (scan SSCC)

--- app/src/main/java/ru/ttmf/mark/home/HomeFragment.java
+++ app/src/main/java/ru/ttmf/mark/home/HomeFragment.java
@@ -1,4 +1,5 @@
 package ru.ttmf.mark.home;
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 import ru.ttmf.mark.R;
 import ru.ttmf.mark.coming.DirectReversePositions;
+import ru.ttmf.mark.scan_sscc.ScanSscc;
 import ru.ttmf.mark.coming.SearchFragment;
 import ru.ttmf.mark.common.BaseFragment;
 import ru.ttmf.mark.common.DataType;
@@ -14,6 +16,7 @@ import ru.ttmf.mark.login.LoginFragment;
 import ru.ttmf.mark.preference.PreferenceController;
 
 import butterknife.OnClick;
+import ru.ttmf.mark.scan_sscc.ScanSsccActivity;
 
 public class HomeFragment extends BaseFragment {
 
@@ -44,6 +47,14 @@ public class HomeFragment extends BaseFragment {
     @OnClick(R.id.inventory)
     public void onInventoryClick() {
         Toast.makeText(getContext(), R.string.inventory_in_dev, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.scan_sscc)
+    public void onScanSsccClick() {
+        //Toast.makeText(getContext(), R.string.scan_sscc_in_dev, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(getContext(), ScanSsccActivity.class);
+        startActivity(intent);
     }
 
 }
