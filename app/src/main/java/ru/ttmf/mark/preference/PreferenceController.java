da7d4d6c3a92c3bccca4445bd4ee6b06089fc235
Albert Danilov <albert95danilov@gmail.com>
2020-09-09 15:20:48 +0300

    ver 1.28 (device version)

--- app/src/main/java/ru/ttmf/mark/preference/PreferenceController.java
+++ app/src/main/java/ru/ttmf/mark/preference/PreferenceController.java
@@ -26,7 +26,7 @@ public class PreferenceController {
     private static final String SERIAL = "serial";
     private static final String LAST_VERSION = "last_version";
     private static final String cur_serial = "cur_serial";
-    private static final Integer cur_version = 128;
+    private static final Integer cur_version = 129;
 
     public List<Sscc_item> sscc_items_list = new ArrayList<Sscc_item>();
 
