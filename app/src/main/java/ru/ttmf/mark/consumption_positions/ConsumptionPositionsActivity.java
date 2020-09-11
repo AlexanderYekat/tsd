21736c4c3d51c97bacff42f0923feb2ca71c315e
Albert Danilov <albert95danilov@gmail.com>
2020-09-11 09:26:12 +0300

    commit

--- app/src/main/java/ru/ttmf/mark/consumption_positions/ConsumptionPositionsActivity.java
+++ app/src/main/java/ru/ttmf/mark/consumption_positions/ConsumptionPositionsActivity.java
@@ -200,10 +200,10 @@ public class ConsumptionPositionsActivity extends ScanActivity implements Observ
     }
 
     private void Scan(List<String> posList, DataMatrix matrix, String code) {
-        //если отсканированнные штрихкоды меньше всего штрихкодов, то продолжить сканирование
+        //если количество отсканированнных штрихкодов меньше необходимого количества, то продолжить сканирование
         int old_scannedCount = scannedCount;
 
-        if (scannedCount != totalCount) {
+        if (scannedCount <= totalCount) {
             //если кол-во отсканированных больше 0, то сохранять только уникальные SGTIN-ы
             if (posList.size() > 0) {
                 if (checkPosition(posList, matrix)) {
@@ -239,7 +239,7 @@ public class ConsumptionPositionsActivity extends ScanActivity implements Observ
                 }
             }
 
-            if (old_scannedCount != scannedCount) {
+            if (old_scannedCount < scannedCount) {
                 if (scannedCount == totalCount) {
                     showSaveDialog(getString(R.string.scan_finish));
                 }
