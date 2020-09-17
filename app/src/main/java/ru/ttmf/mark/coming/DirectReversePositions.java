e4437469e0b49654b2cad5bb45728081e6236181
Albert Danilov <albert95danilov@gmail.com>
2020-09-17 10:03:24 +0300

    update

--- app/src/main/java/ru/ttmf/mark/coming/DirectReversePositions.java
+++ app/src/main/java/ru/ttmf/mark/coming/DirectReversePositions.java
@@ -11,6 +11,7 @@ import butterknife.BindView;
 import ru.ttmf.mark.R;
 import ru.ttmf.mark.common.BaseFragment;
 import ru.ttmf.mark.common.DataType;
+import ru.ttmf.mark.preference.PreferenceController;
 
 
 public class DirectReversePositions extends BaseFragment {
@@ -27,6 +28,10 @@ public class DirectReversePositions extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        PreferenceController.getInstance().setNotFinish(false);
+        PreferenceController.getInstance().setUnScanned(false);
+
         View view = super.onCreateView(inflater, container, savedInstanceState);
         directButton.setOnClickListener(v -> showFragment(SearchFragment.createInstance(DataType.TTN_DIRECT),
                 getString(R.string.direct_coming), true, true));
